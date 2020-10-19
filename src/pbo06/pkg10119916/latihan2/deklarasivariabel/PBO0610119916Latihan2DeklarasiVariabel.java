f94c860f3609952f1126a4251138230dcb9cc59d
DianArpian <>
2020-10-19 23:41:11 +0700

    Deklarasi Variabel

---
+++ src/pbo06/pkg10119916/latihan2/deklarasivariabel/PBO0610119916Latihan2DeklarasiVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo06.pkg10119916.latihan2.deklarasivariabel;
+
+/**
+ * 
+ * @author 
+ * NAMA     : DIAN ARPIAN
+ * KELAS    : IF10K
+ * NIM      : 10119916
+ * Deskripsi Program : Deklarasi Variabel
+ */
+public class PBO0610119916Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //Konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 76;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan Hasil
+        System.out.println();
+        System.out.println(" Isi variabel nilai = " + nilaiInt);
+        System.out.println(" Isi variabel PHI = " + PHI);
+        System.out.println(" Isi Variabel logika = " + nilaiLogika);
+        System.out.println(" Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
